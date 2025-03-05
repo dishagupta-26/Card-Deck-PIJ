451e476c519157c13e448f03430eb7ed335d4215
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-05 20:25:01 +0530

    add menu display and user choice handling in Main class

--- Main.java
+++ Main.java
@@ -9,5 +9,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Deck deck = new Deck();
 
+        while (true) {
+            // display menu
+            System.out.println("\nMenu:");
+            System.out.println("1. Print deck");
+            System.out.println("2. Shuffle deck");
+            System.out.println("3. Deal 5 cards");
+            System.out.println("4. Find a card");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+        }
     }
 }
