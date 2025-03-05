80789dfecd2cfb2a240c42e8e86561c25babe5e2
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-05 20:04:28 +0530

    add createDeck method to Deck class for initializing a standard 52-card deck

--- Deck.java
+++ Deck.java
@@ -10,4 +10,17 @@ public class Deck {
     public Deck() {
         createDeck();
     }
+
+    // create a deck of 52 cards
+    public void createDeck() {
+        String[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        cards = new ArrayList<>();
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+    }
 }
