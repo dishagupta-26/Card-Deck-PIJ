1e59f2fbaa169c475ea705eed93e4ed406eaa270
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-05 20:23:05 +0530

    add main method to Main class for initializing Scanner and Deck instances

--- Main.java
+++ Main.java
@@ -5,5 +5,9 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
 
+    }
 }
