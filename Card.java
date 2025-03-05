2fc491ded638c4612a214c094d978b3cff9ce0c3
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-05 19:42:46 +0530

    add Card class with suit and rank properties

---
+++ Card.java
@@ -0,0 +1,10 @@
+public class Card {
+    private String suit;
+    private String rank;
+
+    // constructor
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
